12cc3c72dd2858982858a1601a13197f32669a29
Awerito <diego@openlink.cl>
2024-09-18 18:36:52 -0300

    Add: user authentication

--- src/app.py
+++ src/app.py
@@ -4,7 +4,9 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
 
+from src.auth import create_admin_user
 from src.config import FASTAPI_CONFIG, MIDDLEWARE_CONFIG, DEVELOPMENT
+from src.routes.auth.auth import router as auth_router
 from src.routes.files.files import router as files_router
 
 
@@ -13,6 +15,11 @@ async def lifespan(_: FastAPI):
     # Start of the application
     if DEVELOPMENT:
         logging.warning("Running in development mode!")
+
+    # Create the admin user if it does not exist
+    user = await create_admin_user()
+    if user:
+        logging.warning("Admin user created!")
 
     yield
 
@@ -24,4 +31,5 @@ app = FastAPI(**FASTAPI_CONFIG, lifespan=lifespan)
 app.add_middleware(CORSMiddleware, **MIDDLEWARE_CONFIG)
 
 # Endpoints
+app.include_router(auth_router, tags=["Users and Authentication"])
 app.include_router(files_router, prefix="/files", tags=["Files"])
