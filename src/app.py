494d50e70f0d9c743aff56322672edacfc3530f3
Awerito <diego@openlink.cl>
2024-09-17 21:09:32 -0300

    Remove: auth endpoints and auth

--- src/app.py
+++ src/app.py
@@ -4,9 +4,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
 
-
-from src.auth import create_admin_user
-from src.routes.auth.auth import authentication_routes
+from src.routes.test.test import router as test_router
 from src.config import FASTAPI_CONFIG, MIDDLEWARE_CONFIG, DEVELOPMENT
 
 
@@ -15,11 +13,6 @@ async def lifespan(_: FastAPI):
     # Start of the application
     if DEVELOPMENT:
         logging.warning("Running in development mode!")
-
-    # Create the admin user if it does not exist
-    user = await create_admin_user()
-    if user:
-        logging.warning("Admin user created!")
 
     yield
 
@@ -31,4 +24,4 @@ app = FastAPI(**FASTAPI_CONFIG, lifespan=lifespan)
 app.add_middleware(CORSMiddleware, **MIDDLEWARE_CONFIG)
 
 # Endpoints
-app.include_router(authentication_routes)
+app.include_router(test_router, prefix="/test")
